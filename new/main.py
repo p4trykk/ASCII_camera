23c76d538ca15e37df2cebe7c15c8fba1d08f3e6
p4trykk <128367670+p4trykk@users.noreply.github.com>
2023-07-05 18:27:44 +0200

    first steps

---
+++ new/main.py
@@ -0,0 +1,10 @@
+from PIL import Image
+
+
+image = Image.open(r"C:\Users\admin\Desktop\IB Patryk\SEMESTR 6\PAzIG_projekt\ASCII_camera\new\DoggoInfobox.jpg")
+
+ascii="Ñ@#W$9876543210?!abc;:+=-,._ ";
+
+ascii_art = convert(image)
+
+
